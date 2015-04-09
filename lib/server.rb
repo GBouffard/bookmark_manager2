92ce3470bb00a6342e9ba2f9809fbd7f73492d0c
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-09 11:16:33 +0100

    Password confirmation test working

--- lib/server.rb
+++ lib/server.rb
@@ -47,7 +47,7 @@ set :sessions_secret, 'super secret'
 
 post '/users' do
   user = User.create(email: params[:email],
-                     password: params[:password])
+                     password: params[:password], password_confirmation:params[:password_confirmation])
   session[:user_id] = user.id
   redirect to('/')
 end
