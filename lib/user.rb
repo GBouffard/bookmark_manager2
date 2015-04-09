70d8b1d05dc607e50e8fc95d6a3bc862f32763e3
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-09 16:08:53 +0100

    validates_uniqueness_of email

--- lib/user.rb
+++ lib/user.rb
@@ -7,17 +7,14 @@ class User
   attr_reader :password
 
   validates_confirmation_of :password
+  validates_uniqueness_of :email
 
   property :id, Serial
-  property :email, String
+  property :email, String, unique: true
   property :password_digest, Text
 
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
-
-
-
-
 end
